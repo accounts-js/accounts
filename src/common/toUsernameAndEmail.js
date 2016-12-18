242d8dbc3864cc7fefbbed087e05ab73309b0119
David Yahalomi <davidyaha@gmail.com>
2016-12-18 15:35:48 +0200

    chore(flow): Added type declarations for accounts and made eslint and flow check happy

--- src/common/toUsernameAndEmail.js
+++ src/common/toUsernameAndEmail.js
@@ -1,4 +1,10 @@
-// Thank you http://stackoverflow.com/a/46181
+/**
+ * Checks if a string is actualy an email.
+ * Thank you http://stackoverflow.com/a/46181.
+ *
+ * @param {string} email - presumably an email
+ * @returns {boolean} whether or not it is an email
+ */
 function isEmail(email) {
   // eslint-disable-next-line
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
