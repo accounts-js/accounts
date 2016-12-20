750fa0ea19ae6a8fc9db583b655f3dcd239bc760
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-12-19 19:17:04 -0800

    Server side Accounts.createUser

--- src/server/DBDriver.js
+++ src/server/DBDriver.js
@@ -1,3 +1,12 @@
+// @flow
+import type UserObjectType from '../common/UserObjectType';
+
+export interface DBDriverInterface {
+  findUserByEmail(email: string): Promise<UserObjectType> | Promise<null>,
+  findUserByUsername(username: string): Promise<UserObjectType> | Promise<null>,
+  createUser(user: UserObjectType): Promise<string>
+}
+
 class DBDriver {
 
 }
