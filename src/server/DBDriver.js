c0a7b07910151416e22585731ce3db0a3f3f8053
Tim Mikeladze <tim.mikeladze@gmail.com>
2017-01-08 21:08:52 -0800

    Updated DBDriver return types

--- src/server/DBDriver.js
+++ src/server/DBDriver.js
@@ -1,4 +1,5 @@
 // @flow
+/* eslint-disable max-len */
 import type UserObjectType from '../common/UserObjectType';
 // import AccountsServer from '../server/AccountsServer';
 import { hashPassword, verifyPassword } from './encryption';
@@ -8,8 +9,8 @@ class DBDriver {
   verifyPassword: (password: string, hash: string) => Promise<boolean>
   findPasswordHash: (userId: string) => Promise<?string>
   createUser: (user: UserObjectType) => Promise<string>
-  findUserByEmail: (email: string, onlyId: ?boolean) => Promise<?UserObjectType>
-  findUserByUsername: (username: string, onlyId: ?boolean) => Promise<?UserObjectType>
+  findUserByEmail: (email: string, onlyId: ?boolean) => Promise<UserObjectType | string | null>
+  findUserByUsername: (username: string, onlyId: ?boolean) => Promise<UserObjectType | string | null>
   findUserById: (userId: string) => Promise<?UserObjectType>
   constructor() {
     this.hashPassword = hashPassword;
