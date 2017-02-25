d112878a1e4bb6960c9da3bc889c4d4c1e44e974
dotansimha <dotansimha@gmail.com>
2017-02-25 12:54:14 +0200

    feat(logout): implement logout mutation

--- packages/graphql-api/packages/graphql-client/src/graphql/logout.mutation.js
+++ packages/graphql-api/packages/graphql-client/src/graphql/logout.mutation.js
@@ -1,1 +1,7 @@
-export const logoutMutation = '';
+import gql from 'graphql-tag';
+
+export const logoutMutation = gql`
+  mutation logout($accessToken: String!) {
+      logout(accessToken: $accessToken)
+  }
+`;
