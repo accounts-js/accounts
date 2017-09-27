75fef54e30be6995455edb1e0cd8e6478ca83f90
dotansimha <dotansimha@gmail.com>
2017-09-27 09:13:34 +0300

    fix for context

--- packages/graphql-api/packages/graphql-api/src/resolvers/refresh-tokens.ts
+++ packages/graphql-api/packages/graphql-api/src/resolvers/refresh-tokens.ts
@@ -1,5 +1,5 @@
 export const refreshTokens = Accounts =>
-  (async (_, { accessToken, refreshToken }, { context }) => {
+  (async (_, { accessToken, refreshToken }, context) => {
     const result = await Accounts.refreshTokens(accessToken, refreshToken);
 
     if (result && result.user) {
