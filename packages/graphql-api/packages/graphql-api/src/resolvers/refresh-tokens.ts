45806a10fdb24b1b30d0924aa195320b8a2dfcf1
dotansimha <dotansimha@gmail.com>
2017-09-27 09:11:49 +0300

    fix missing user on refresh token call

--- packages/graphql-api/packages/graphql-api/src/resolvers/refresh-tokens.ts
+++ packages/graphql-api/packages/graphql-api/src/resolvers/refresh-tokens.ts
@@ -1,3 +1,10 @@
 export const refreshTokens = Accounts =>
-  (async (_, { accessToken, refreshToken }) =>
-    await Accounts.refreshTokens(accessToken, refreshToken));
+  (async (_, { accessToken, refreshToken }, { context }) => {
+    const result = await Accounts.refreshTokens(accessToken, refreshToken);
+
+    if (result && result.user) {
+      context.user = result.user;
+    }
+
+    return result;
+  });
