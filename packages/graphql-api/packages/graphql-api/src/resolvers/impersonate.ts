d2205a64954088339a93f8c2ec4eedbaaabbd265
dotansimha <dotansimha@gmail.com>
2017-09-28 17:39:57 +0300

    fix for impersonate context

--- packages/graphql-api/packages/graphql-api/src/resolvers/impersonate.ts
+++ packages/graphql-api/packages/graphql-api/src/resolvers/impersonate.ts
@@ -1,3 +1,10 @@
 export const impersonate = Accounts =>
-  (async (_, { accessToken, username }) =>
-    await Accounts.impersonate(accessToken, username));
+  (async (_, { accessToken, username }, context) => {
+    const result = await await Accounts.impersonate(accessToken, username);
+
+    if (result && result.user) {
+      context.user = result.user;
+    }
+
+    return result;
+  });
