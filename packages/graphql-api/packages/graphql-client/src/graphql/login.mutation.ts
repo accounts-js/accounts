6c2f61591fdba4e038acef92a10cd61e55eef077
dotansimha <dotansimha@gmail.com>
2017-09-28 16:34:03 +0300

    fix for client login

--- packages/graphql-api/packages/graphql-client/src/graphql/login.mutation.ts
+++ packages/graphql-api/packages/graphql-client/src/graphql/login.mutation.ts
@@ -2,8 +2,8 @@ import gql from 'graphql-tag';
 import { loginFieldsFragment } from './login-fields.fragment';
 
 export const createLoginMutation = userFieldsFragment => gql`
-  mutation($user: UserInput!, $password: String!) {
-    loginWithPassword(user: $user, password: $password) {
+  mutation($user: String, $userFields: UserInput, $password: String!) {
+    loginWithPassword(user: $user, userFields: $userFields, password: $password) {
       sessionId
       ...LoginFields
       user {
