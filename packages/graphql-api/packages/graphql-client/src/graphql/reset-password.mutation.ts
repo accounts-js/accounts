675f655a7d54042b903ff22cf9e77aafd5a94166
DAB0mB <emanor6@gmail.com>
2017-10-28 03:07:56 +0300

    Use the right parameter name

--- packages/graphql-api/packages/graphql-client/src/graphql/reset-password.mutation.ts
+++ packages/graphql-api/packages/graphql-client/src/graphql/reset-password.mutation.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const resetPasswordMutation = gql`
-    mutation($token: String!, $password: PasswordInput!) {
-        resetPassword(token: $token, password: $password)
+    mutation($token: String!, $newPassword: PasswordInput!) {
+        resetPassword(token: $token, newPassword: $password)
     }
 `;
