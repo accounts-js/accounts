cfc389fce662c2c4f8052ede93397107a3d501b4
Pradel Léo <pradel.leo@gmail.com>
2017-11-17 13:43:58 +0100

    Add options to README

--- packages/accounts-mongo/src/index.ts
+++ packages/accounts-mongo/src/index.ts
@@ -3,16 +3,24 @@ import { get } from 'lodash';
 import { CreateUserType, UserObjectType, SessionType } from '@accounts/server';
 
 export interface MongoOptionsType {
+  // The users collection name, default 'users'.
   collectionName?: string;
+  // The sessions collection name, default 'sessions'.
   sessionCollectionName?: string;
+  // The timestamps for the users and sessions collection, default 'createdAt' and 'updatedAt'.
   timestamps?: {
     createdAt: string;
     updatedAt: string;
   };
+  // Should the user collection use _id as string or ObjectId, default 'true'.
   convertUserIdToMongoObjectId?: boolean;
+  // Should the session collection use _id as string or ObjectId, default 'true'.
   convertSessionIdToMongoObjectId?: boolean;
+  // Perform case intensitive query for user name, default 'true'.
   caseSensitiveUserName?: boolean;
+  // Function that generate the id for new objects.
   idProvider?: () => string | object;
+  // Function that generate the date for the timestamps.
   dateProvider?: (date?: Date) => any;
 }
 
