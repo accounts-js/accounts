12ac51ae4bc7d8325afe59a0a9aeea1b92aba45b
Pradel Léo <pradel.leo@gmail.com>
2017-12-04 18:41:47 +0100

    remove instagram option

--- packages/oauth-instagram/src/index.ts
+++ packages/oauth-instagram/src/index.ts
@@ -1,17 +1,6 @@
 import * as rp from 'request-promise';
 
-export interface AccountsOauthTwitterOptions {
-  key: string;
-  secret: string;
-}
-
 class AccountsOauthTwitter {
-  private options: AccountsOauthTwitterOptions;
-
-  constructor(options: AccountsOauthTwitterOptions) {
-    this.options = options;
-  }
-
   public async authenticate(params) {
     let data = await rp(`https://api.instagram.com/v1/users/self/?access_token=${params.access_token}`);
     data = JSON.parse(data).data;
