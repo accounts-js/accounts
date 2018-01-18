ce12725d9847309bf354264c000a2719d59bfd8a
pradel <pradel.leo@gmail.com>
2018-01-18 18:03:08 +0100

    activate oauth twitter tests

---
+++ packages/oauth-twitter/__tests__/index.ts
@@ -0,0 +1,7 @@
+import AccountsOAuthTwitter from '../src';
+
+describe('AccountsOAuthTwitter', () => {
+  it('should have default export AccountsOAuthTwitter', () => {
+    expect(typeof AccountsOAuthTwitter).toBe('function');
+  })
+})
