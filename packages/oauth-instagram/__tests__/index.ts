686c07df9a17ad4773a4c9694fdf4c7eac92f15e
pradel <pradel.leo@gmail.com>
2018-01-18 18:00:55 +0100

    activate oauth instagram tests

---
+++ packages/oauth-instagram/__tests__/index.ts
@@ -0,0 +1,7 @@
+import AccountsOAuthInstagram from '../src';
+
+describe('AccountsOAuthInstagram', () => {
+  it('should have default export AccountsOAuthInstagram', () => {
+    expect(typeof AccountsOAuthInstagram).toBe('function');
+  })
+})
