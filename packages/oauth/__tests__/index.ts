67dc11e7d5e0cb14ff9f18696c3dd54826528068
pradel <pradel.leo@gmail.com>
2018-01-19 16:45:17 +0100

    test index files

---
+++ packages/oauth/__tests__/index.ts
@@ -0,0 +1,7 @@
+import AccountsOauth from '../src';
+
+describe('AccountsOauth', () => {
+  it('should have default export AccountsOauth', () => {
+    expect(typeof AccountsOauth).toBe('function');
+  })
+})
---
+++ packages/password/__tests__/index.ts
@@ -0,0 +1,7 @@
+import AccountsPassword from '../src';
+
+describe('AccountsPassword', () => {
+  it('should have default export AccountsPassword', () => {
+    expect(typeof AccountsPassword).toBe('function');
+  })
+})
