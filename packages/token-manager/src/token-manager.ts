6735d658e46ffc12dec7a22f8dc00855e3a56bc8
Elies Lou <elieslou.dev@gmail.com>
2018-03-13 15:28:20 +0100

    @accounts/token-manager : class initialisation and configuration validation

--- packages/token-manager/src/token-manager.ts
+++ packages/token-manager/src/token-manager.ts
@@ -18,9 +18,29 @@ const defaultRefreshTokenConfig: TokenGenerationConfiguration = {
 };
 
 export default class TokenManager {
+
   private secret: string;
+
   private emailTokenExpiration: number;
+
   private accessTokenConfig: TokenGenerationConfiguration;
+
   private refreshTokenConfig: TokenGenerationConfiguration;
 
+  constructor(config: Configuration) {
+    this.validateConfiguration(config);
+    this.secret = config.secret;
+    this.emailTokenExpiration = config.emailTokenExpiration || 1000 * 60;
+    this.accessTokenConfig = { ...defaultTokenConfig, ...defaultAccessTokenConfig, ...config.access };
+    this.refreshTokenConfig = { ...defaultTokenConfig, ...defaultRefreshTokenConfig, ...config.refresh };
+  }
+
+  private validateConfiguration(config: Configuration): void {
+    if (!config) {
+      throw new Error('[ Accounts - TokenManager ] configuration : A configuration object is needed');
+    }
+    if (typeof config.secret !== 'string') {
+      throw new Error('[ Accounts - TokenManager ] configuration : A string secret property is needed');
+    }
+  }
 }
