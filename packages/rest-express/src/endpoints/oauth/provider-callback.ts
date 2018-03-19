8909514c35f3bf839803e40582952b3e93e30a41
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-03-19 17:34:52 +0100

    Add onOAuthSuccess, onOAuthError, transformOAuthResponse callbacks

--- packages/rest-express/src/endpoints/oauth/provider-callback.ts
+++ packages/rest-express/src/endpoints/oauth/provider-callback.ts
@@ -3,15 +3,16 @@ import * as requestIp from 'request-ip';
 import { AccountsServer } from '@accounts/server';
 import { getUserAgent } from '../../utils/get-user-agent';
 import { sendError } from '../../utils/send-error';
+import { AccountsExpressOptions } from '../../types';
 
 interface RequestWithSession extends express.Request {
   session: { [key: string]: any };
 }
 
-export const providerCallback = (accountsServer: AccountsServer) => async (
-  req: express.Request,
-  res: express.Response
-) => {
+export const providerCallback = (
+  accountsServer: AccountsServer,
+  options?: AccountsExpressOptions
+) => async (req: express.Request, res: express.Response) => {
   try {
     const userAgent = getUserAgent(req);
     const ip = requestIp.getClientIp(req);
@@ -25,8 +26,21 @@ export const providerCallback = (accountsServer: AccountsServer) => async (
       },
       { ip, userAgent }
     );
-    res.json(loggedInUser);
+
+    if (options && options.onOAuthSuccess) {
+      options.onOAuthSuccess(req, res, loggedInUser);
+    }
+
+    if (options && options.transformOAuthResponse) {
+      res.json(options.transformOAuthResponse(loggedInUser));
+    } else {
+      res.json(loggedInUser);
+    }
   } catch (err) {
+    if (options && options.onOAuthError) {
+      options.onOAuthError(req, res, err);
+    }
+
     sendError(res, err);
   }
 };
