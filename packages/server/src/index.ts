cb4592ad897917a5ad01a3085b235f09b52c8939
pradel <pradel.leo@gmail.com>
2018-03-20 12:38:07 +0100

    export ServerHooks keys

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,11 +2,13 @@ import { AccountsServer } from './accounts-server';
 import * as encryption from './utils/encryption';
 import { generateRandomToken } from './utils/tokens';
 import { getFirstUserEmail } from './utils/get-first-user-email';
+import { ServerHooks } from './utils/server-hooks';
 
 export default AccountsServer;
 export {
   AccountsServer,
   encryption,
+  ServerHooks,
   generateRandomToken,
   getFirstUserEmail
 };
