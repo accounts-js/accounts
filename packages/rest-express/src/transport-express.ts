9bd3c1ca51711ba5342789ddf55d37053b601f6b
Elies Lou <elieslou.dev@gmail.com>
2018-04-04 18:33:20 +0200

    Express: remove static routes$

--- packages/rest-express/src/transport-express.ts
+++ packages/rest-express/src/transport-express.ts
@@ -32,14 +32,14 @@ export default class TransportExpress {
 			.post(`/${this.path}/refreshTokens`, this.refreshTokens)
 			.post(`/${this.path}/logout`, this.logout)
       // .post(`/${this.path}/:service/:authenticate`, this.serviceAuthenticate)
-      .post(`/${this.path}/password/register`, this.registerPassword)
-      .post(`/${this.path}/password/verifyEmail`, this.verifyEmail)
-      .post(`/${this.path}/password/resetPassword`, this.resetPassword)
-      .post(`/${this.path}/password/sendVerificationEmail`, this.sendVerificationEmail)
-      .post(`/${this.path}/password/sendResetPasswordEmail`, this.sendResetPasswordEmail)
-      .post(`/${this.path}/password/twoFactorSecret`, this.userLoader, this.twoFactorSecret)
-      .post(`/${this.path}/password/twoFactorSet`, this.userLoader, this.twoFactorSet)
-      .post(`/${this.path}/password/twoFactorUnset`, this.userLoader, this.twoFactorUnset)
+      // .post(`/${this.path}/password/register`, this.registerPassword)
+      // .post(`/${this.path}/password/verifyEmail`, this.verifyEmail)
+      // .post(`/${this.path}/password/resetPassword`, this.resetPassword)
+      // .post(`/${this.path}/password/sendVerificationEmail`, this.sendVerificationEmail)
+      // .post(`/${this.path}/password/sendResetPasswordEmail`, this.sendResetPasswordEmail)
+      // .post(`/${this.path}/password/twoFactorSecret`, this.userLoader, this.twoFactorSecret)
+      // .post(`/${this.path}/password/twoFactorSet`, this.userLoader, this.twoFactorSet)
+      // .post(`/${this.path}/password/twoFactorUnset`, this.userLoader, this.twoFactorUnset)
       // .post(`/${this.path}/oauth/:provider/callback`, this.providerCallback)
 			.post(`/${this.path}/:service/:provider?/:action`, this.useService)
   }
@@ -141,104 +141,104 @@ export default class TransportExpress {
   // };
 
 
-  // PASSWORD
-  private registerPassword = async ( req: Request, res: Response ) => {
-    try {
-      const password: any = this.accountsServer.getServices().password;
-      const userId = await password.createUser(req.body.user);
-      res.json({ userId });
-    } catch (err) {
-      this.sendError(res, err);
-    }
-  };
+  // // PASSWORD
+  // private registerPassword = async ( req: Request, res: Response ) => {
+  //   try {
+  //     const password: any = this.accountsServer.getServices().password;
+  //     const userId = await password.createUser(req.body.user);
+  //     res.json({ userId });
+  //   } catch (err) {
+  //     this.sendError(res, err);
+  //   }
+  // };
 
-  private resetPassword = async ( req: Request, res: Response ) => {
-    try {
-      const { token, newPassword } = req.body;
-      const password: any = this.accountsServer.getServices().password;
-      await password.resetPassword(token, newPassword);
-      res.json({ message: 'Password changed' });
-    } catch (err) {
-      this.sendError(res, err);
-    }
-  };
+  // private resetPassword = async ( req: Request, res: Response ) => {
+  //   try {
+  //     const { token, newPassword } = req.body;
+  //     const password: any = this.accountsServer.getServices().password;
+  //     await password.resetPassword(token, newPassword);
+  //     res.json({ message: 'Password changed' });
+  //   } catch (err) {
+  //     this.sendError(res, err);
+  //   }
+  // };
 
-  private sendResetPasswordEmail = async ( req: Request, res: Response ) => {
-    try {
-      const { email } = req.body;
-      const password: any = this.accountsServer.getServices().password;
-      await password.sendResetPasswordEmail(email);
-      res.json({ message: 'Email sent' });
-    } catch (err) {
-      this.sendError(res, err);
-    }
-  };
+  // private sendResetPasswordEmail = async ( req: Request, res: Response ) => {
+  //   try {
+  //     const { email } = req.body;
+  //     const password: any = this.accountsServer.getServices().password;
+  //     await password.sendResetPasswordEmail(email);
+  //     res.json({ message: 'Email sent' });
+  //   } catch (err) {
+  //     this.sendError(res, err);
+  //   }
+  // };
 
-  private verifyEmail = async ( req: Request, res: Response ) => {
-    try {
-      const { token } = req.body;
-      const password: any = this.accountsServer.getServices().password;
-      await password.verifyEmail(token);
-      res.json({ message: 'Email verified' });
-    } catch (err) {
-      this.sendError(res, err);
-    }
-  };
+  // private verifyEmail = async ( req: Request, res: Response ) => {
+  //   try {
+  //     const { token } = req.body;
+  //     const password: any = this.accountsServer.getServices().password;
+  //     await password.verifyEmail(token);
+  //     res.json({ message: 'Email verified' });
+  //   } catch (err) {
+  //     this.sendError(res, err);
+  //   }
+  // };
 
-  private sendVerificationEmail = async ( req: Request, res: Response ) => {
-    try {
-      const { email } = req.body;
-      const password: any = this.accountsServer.getServices().password;
-      await password.sendVerificationEmail(email);
-      res.json({ message: 'Email sent' });
-    } catch (err) {
-      this.sendError(res, err);
-    }
-  };
+  // private sendVerificationEmail = async ( req: Request, res: Response ) => {
+  //   try {
+  //     const { email } = req.body;
+  //     const password: any = this.accountsServer.getServices().password;
+  //     await password.sendVerificationEmail(email);
+  //     res.json({ message: 'Email sent' });
+  //   } catch (err) {
+  //     this.sendError(res, err);
+  //   }
+  // };
 
-  private  twoFactorSecret = async ( req: Request, res: Response ) => {
-    try {
-      const password: any = this.accountsServer.getServices().password;
-      const secret = await password.twoFactor.getNewAuthSecret();
-      res.json({ secret });
-    } catch (err) {
-      this.sendError(res, err);
-    }
-  };
+  // private  twoFactorSecret = async ( req: Request, res: Response ) => {
+  //   try {
+  //     const password: any = this.accountsServer.getServices().password;
+  //     const secret = await password.twoFactor.getNewAuthSecret();
+  //     res.json({ secret });
+  //   } catch (err) {
+  //     this.sendError(res, err);
+  //   }
+  // };
 
-  private twoFactorSet = async ( req: Request, res: Response ) => {
-    try {
-      if (!(req as any).userId) {
-        res.status(401);
-        res.json({ message: 'Unauthorized' });
-        return;
-      }
-      const password: any = this.accountsServer.getServices().password;
-      const secret = await password.twoFactor.set(
-        (req as any).userId,
-        req.body.secret,
-        req.body.code
-      );
-      res.json({});
-    } catch (err) {
-      this.sendError(res, err);
-    }
-  };
+  // private twoFactorSet = async ( req: Request, res: Response ) => {
+  //   try {
+  //     if (!(req as any).userId) {
+  //       res.status(401);
+  //       res.json({ message: 'Unauthorized' });
+  //       return;
+  //     }
+  //     const password: any = this.accountsServer.getServices().password;
+  //     const secret = await password.twoFactor.set(
+  //       (req as any).userId,
+  //       req.body.secret,
+  //       req.body.code
+  //     );
+  //     res.json({});
+  //   } catch (err) {
+  //     this.sendError(res, err);
+  //   }
+  // };
   
-  private twoFactorUnset = async ( req: Request, res: Response ) => {
-    try {
-      if (!(req as any).userId) {
-        res.status(401);
-        res.json({ message: 'Unauthorized' });
-        return;
-      }
-      const password: any = this.accountsServer.getServices().password;
-      const secret = await password.twoFactor.unset((req as any).userId, req.body.code);
-      res.json({});
-    } catch (err) {
-      this.sendError(res, err);
-    }
-  };
+  // private twoFactorUnset = async ( req: Request, res: Response ) => {
+  //   try {
+  //     if (!(req as any).userId) {
+  //       res.status(401);
+  //       res.json({ message: 'Unauthorized' });
+  //       return;
+  //     }
+  //     const password: any = this.accountsServer.getServices().password;
+  //     const secret = await password.twoFactor.unset((req as any).userId, req.body.code);
+  //     res.json({});
+  //   } catch (err) {
+  //     this.sendError(res, err);
+  //   }
+  // };
   
   // private providerCallback = async ( req: Request, res: Response ) => {
   //   try {
