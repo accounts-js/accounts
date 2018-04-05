348a87b4b49a668a65859079963602c42ee9658a
Elies Lou <elieslou.dev@gmail.com>
2018-04-05 11:26:39 +0200

    Password: return Message instead of void

--- packages/password/src/accounts-password.ts
+++ packages/password/src/accounts-password.ts
@@ -1,5 +1,15 @@
+import { 
+  CreateUser,
+  User,
+  Login,
+  EmailRecord,
+  TokenRecord,
+  DatabaseInterface,
+  AuthenticationService,
+  ConnectionInformations,
+  Message
+} from '@accounts/types';
 import { trim, isEmpty, isFunction, isString, isPlainObject, get, find, includes } from 'lodash';
-import { CreateUser, User, Login, EmailRecord, TokenRecord, DatabaseInterface, AuthenticationService, ConnectionInformations } from '@accounts/types';
 import { HashAlgorithm } from '@accounts/common';
 import { TwoFactor, AccountsTwoFactorOptions } from '@accounts/two-factor';
 import { AccountsServer, getFirstUserEmail } from '@accounts/server';
@@ -154,7 +164,7 @@ export default class AccountsPassword implements AuthenticationService {
    * @param {string} token - The token retrieved from the verification URL.
    * @returns {Promise<void>} - Return a Promise.
    */
-  public async verifyEmail({ token } : { token: string }): Promise<void> {
+  public async verifyEmail({ token } : { token: string }): Promise<Message> {
     const user = await this.db.findUserByEmailVerificationToken(token);
     if (!user) {
       throw new Error('Verify email link expired');
@@ -175,6 +185,7 @@ export default class AccountsPassword implements AuthenticationService {
       throw new Error('Verify email link is for unknown address');
     }
     await this.db.verifyEmail(user.id, emailRecord.address);
+    return { message: 'Email verified' }
   }
 
   /**
@@ -183,7 +194,7 @@ export default class AccountsPassword implements AuthenticationService {
    * @param {string} newPassword - A new password for the user.
    * @returns {Promise<void>} - Return a Promise.
    */
-  public async resetPassword({ token, newPassword } : { token: string, newPassword: PasswordType }): Promise<void> {
+  public async resetPassword({ token, newPassword } : { token: string, newPassword: PasswordType }): Promise<Message> {
     const user = await this.db.findUserByResetPasswordToken(token);
     if (!user) {
       throw new Error('Reset password link expired');
@@ -207,6 +218,7 @@ export default class AccountsPassword implements AuthenticationService {
     await this.db.setResetPassword(user.id, resetTokenRecord.address, password, token);
     // Changing the password should invalidate existing sessions
     this.db.invalidateAllSessions(user.id);
+    return {message: 'Password Changed'}
   }
 
   /**
@@ -227,7 +239,7 @@ export default class AccountsPassword implements AuthenticationService {
    * Defaults to the first unverified email in the list.
    * @returns {Promise<void>} - Return a Promise.
    */
-  public async sendVerificationEmail({ address }: { address: string }): Promise<void> {
+  public async sendVerificationEmail({ address }: { address: string }): Promise<Message> {
     if (!address) {
       throw new Error('Invalid email');
     }
@@ -253,6 +265,7 @@ export default class AccountsPassword implements AuthenticationService {
     );
 
     await this.server.options.sendMail(resetPasswordMail);
+    return { message: 'Email Sent' }
   }
 
   /**
@@ -262,7 +275,7 @@ export default class AccountsPassword implements AuthenticationService {
    * Defaults to the first email in the list.
    * @returns {Promise<void>} - Return a Promise.
    */
-  public async sendResetPasswordEmail({ address }: { address: string }): Promise<void> {
+  public async sendResetPasswordEmail({ address }: { address: string }): Promise<Message> {
     if (!address) {
       throw new Error('Invalid email');
     }
@@ -284,6 +297,7 @@ export default class AccountsPassword implements AuthenticationService {
     );
 
     await this.server.options.sendMail(resetPasswordMail);
+    return { message: 'Email sent' }
   }
 
   /**
@@ -293,7 +307,7 @@ export default class AccountsPassword implements AuthenticationService {
    * Defaults to the first email in the list.
    * @returns {Promise<void>} - Return a Promise.
    */
-  public async sendEnrollmentEmail({ address }: { address: string }): Promise<void> {
+  public async sendEnrollmentEmail({ address }: { address: string }): Promise<Message> {
     const user = await this.db.findUserByEmail(address);
     if (!user) {
       throw new Error('User not found');
@@ -312,6 +326,7 @@ export default class AccountsPassword implements AuthenticationService {
     );
 
     await this.server.options.sendMail(enrollmentMail);
+    return { message: 'Email Sent' }
   }
 
   /**
