d4feeaf0aef7e8ae7fef5b5591500a635ee69780
Elies Lou <elieslou.dev@gmail.com>
2018-04-05 14:19:19 +0200

    ETTC: fix fileNames

--- packages/express-tt-cookies/__tests__/index.ts
+++ packages/express-tt-cookies/__tests__/index.ts
@@ -1,7 +1,7 @@
-import ExpressTTHeaders from '../src';
+import ExpressTTCookies from '../src';
 
-describe('ExpressTTHeaders entry', () => {
-  it('should have default export ExpressTTHeaders', () => {
-    expect(typeof ExpressTTHeaders).toBe('function');
+describe('ExpressTTCookies entry', () => {
+  it('should have default export ExpressTTCookies', () => {
+    expect(typeof ExpressTTCookies).toBe('function');
   });
 });
