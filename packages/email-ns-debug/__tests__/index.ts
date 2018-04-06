aa6b12495e5aa490ccad8f197c83ac364e544041
Elies Lou <elieslou.dev@gmail.com>
2018-04-06 13:22:12 +0200

    EmailNSDebug: Add the package to debug emails

---
+++ packages/email-ns-debug/__tests__/index.ts
@@ -0,0 +1,7 @@
+import EmailNSDebug from '../src';
+
+describe('EmailNSDebug entry', () => {
+  it('should have default export EmailNSDebug', () => {
+    expect(typeof EmailNSDebug).toBe('function');
+  });
+});
