89caf2b08e67aedc49e9431d8552ca6dc37505bd
Elies Lou <elieslou.dev@gmail.com>
2018-04-08 13:30:26 +0200

    ClientRestTTBody: fix test name

--- packages/client-rest-tt-body/__tests__/index.ts
+++ packages/client-rest-tt-body/__tests__/index.ts
@@ -1,7 +1,7 @@
-import ClientRestTTHeaders from '../src';
+import ClientRestTTBody from '../src';
 
-describe('ClientRestTTHeaders entry', () => {
-  it('should have default export ClientRestTTHeaders', () => {
-    expect(typeof ClientRestTTHeaders).toBe('function');
+describe('ClientRestTTBody entry', () => {
+  it('should have default export ClientRestTTBody', () => {
+    expect(typeof ClientRestTTBody).toBe('function');
   });
 });
