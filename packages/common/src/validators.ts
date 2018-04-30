7213a1f40b32f1f33d2f733b9903123bdd08eb4a
pradel <pradel.leo@gmail.com>
2018-04-30 18:35:20 +0200

    fix lodash pb with jest

--- packages/common/src/validators.ts
+++ packages/common/src/validators.ts
@@ -1,6 +1,4 @@
-// tslint:disable no-submodule-imports
-import isEmpty from 'lodash-es/isEmpty';
-import trim from 'lodash-es/trim';
+import { isEmpty, trim } from 'lodash';
 
 export const isEmail = (email?: string) => {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
