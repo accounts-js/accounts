4019b54721cf5bafa08f49a928e3303bfc9935e5
pradel <pradel.leo@gmail.com>
2018-05-30 12:51:33 +0200

    add comments

--- packages/client-password/src/types/options.ts
+++ packages/client-password/src/types/options.ts
@@ -1,7 +1,7 @@
 export interface AccountsClientPasswordOptions {
   /**
-   * Use this method if you want to customize the hashing method
-   * By default accounts-js will hash the password using SHA256
+   * Use this option if you want to customize the hashing method.
+   * By default accounts-js will hash the password using SHA256.
    */
   hashPassword?(password: string): string;
 }
