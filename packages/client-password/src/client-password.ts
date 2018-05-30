032e9ce8155e65d07da8da72cb7315ae7c50348c
pradel <pradel.leo@gmail.com>
2018-05-30 12:36:30 +0200

    Add hashPassword option

--- packages/client-password/src/client-password.ts
+++ packages/client-password/src/client-password.ts
@@ -3,14 +3,22 @@ import { SHA256 } from 'crypto-js';
 
 // TODO option for the user to change hash algo
 
-// interface AccountsClientPasswordOptions {}
+export interface AccountsClientPasswordOptions {
+  /**
+   * Use this method if you want to customize the hashing method
+   * By default accounts-js will hash the password using SHA256
+   */
+  hashPassword?(password: string): string;
+}
 
 export class AccountsClientPassword {
   private client: AccountsClient;
+  private options: AccountsClientPasswordOptions;
 
-  constructor(client: AccountsClient) {
+  constructor(client: AccountsClient, options?: AccountsClientPasswordOptions) {
     // TODO check client or throw error
     this.client = client;
+    this.options = options;
   }
 
   // TODO type
@@ -46,6 +54,9 @@ export class AccountsClientPassword {
   }
 
   public hashPassword(password: string): string {
+    if (this.options.hashPassword) {
+      return this.options.hashPassword(password);
+    }
     const hashedPassword = SHA256(password);
     return hashedPassword.toString();
   }
