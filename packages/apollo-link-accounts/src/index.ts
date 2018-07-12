e62d0d80060486b926e2edbcc126838e161ac001
Léo Pradel <pradel.leo@gmail.com>
2018-07-12 14:59:49 +0200

    Fix accounts apollo-link types (#382)

--- packages/apollo-link-accounts/src/index.ts
+++ packages/apollo-link-accounts/src/index.ts
@@ -1,7 +1,8 @@
 import { setContext } from 'apollo-link-context';
+import { ApolloLink } from 'apollo-link';
 import { AccountsClient } from '@accounts/client';
 
-export const accountsLink = (accountsClient: AccountsClient) => {
+export const accountsLink = (accountsClient: AccountsClient): ApolloLink => {
   return setContext(async (_, { headers }) => {
     const tokens = await accountsClient.refreshSession();
     return {
