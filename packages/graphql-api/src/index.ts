3a9e5b8a32fa9019cf700aa79611142f7460ad40
Kun Ran <rankun203@gmail.com>
2018-07-18 09:07:47 +0800

    feat: export graphql-api types

--- packages/graphql-api/src/index.ts
+++ packages/graphql-api/src/index.ts
@@ -3,3 +3,6 @@ import { authenticated } from './utils/authenticated-resolver';
 import { accountsContext } from './utils/context-builder';
 
 export { createAccountsGraphQL, authenticated, accountsContext };
+
+export * from './types/graphql';
+export * from './types';
