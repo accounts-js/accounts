1e0fb6bde01778c0094b9556ebe4acddb2b27dda
pradel <pradel.leo@gmail.com>
2018-07-20 15:41:58 +0200

    two-factor ErrorMessages

--- packages/two-factor/src/types/options.ts
+++ packages/two-factor/src/types/options.ts
@@ -1,7 +1,23 @@
+export interface ErrorMessages {
+  userNotFound: string;
+  codeDidNotMatch: string;
+  userTwoFactorNotSet: string;
+  userTwoFactorAlreadySet: string;
+  codeRequired: string;
+}
+
 export interface AccountsTwoFactorOptions {
-  // Two factor app name
+  /**
+   * Two factor app name
+   */
   appName?: string;
-  // Two factor secret length, default to 20
+  /**
+   * Two factor secret length, default to 20
+   */
   secretLength?: number;
   window?: number;
+  /**
+   * Two factor module errors
+   */
+  errors?: ErrorMessages;
 }
