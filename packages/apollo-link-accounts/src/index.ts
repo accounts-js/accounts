b7a60a8277bdff08fc34677dab217d2e9c565f99
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-12-04 05:19:25 -0500

    Use factory function to handle circular import

--- packages/apollo-link-accounts/src/index.ts
+++ packages/apollo-link-accounts/src/index.ts
@@ -1,14 +1,18 @@
 import { setContext } from 'apollo-link-context';
 import { ApolloLink } from 'apollo-link';
+import { AccountsClient } from '@accounts/client';
 
-export const accountsLink = ({ headerName = 'accounts-access-token' }): ApolloLink => {
+type AccountsClientFactory = () => AccountsClient | Promise<AccountsClient>;
+
+export const accountsLink = (accountsClientFactory: AccountsClientFactory): ApolloLink => {
   return setContext(async (_, { headers: headersWithoutTokens }) => {
+    const accountsClient = await accountsClientFactory();
+    const tokens = await accountsClient.refreshSession();
+
     const headers = { ...headersWithoutTokens };
 
-    const token = localStorage.getItem('accounts:accessToken');
-
-    if (token) {
-      headers[headerName] = token;
+    if (tokens) {
+      headers['accounts-access-token'] = tokens.accessToken;
     }
 
     return {
