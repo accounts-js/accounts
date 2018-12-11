23afb9bd89622aa9baa16f66d4a924cb2bb39516
pradel <pradel.leo@gmail.com>
2018-12-11 17:18:02 +0100

    test accounts-password resolvers query

---
+++ packages/graphql-api/__tests__/modules/accounts-password/resolvers/query.ts
@@ -0,0 +1,35 @@
+import { AccountsPassword } from '@accounts/password';
+import { Query } from '../../../../src/modules/accounts-password/resolvers/query';
+
+describe('accounts-password resolvers query', () => {
+  const accountsPasswordMock = {
+    twoFactor: {
+      getNewAuthSecret: jest.fn(),
+    },
+  };
+  const injector = {
+    get: jest.fn(() => accountsPasswordMock),
+  };
+  const user = { id: 'idTest' };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('twoFactorSecret', () => {
+    it('should throw if no user in context', async () => {
+      try {
+        await Query.twoFactorSecret!({}, {}, {} as any, {} as any);
+        throw new Error();
+      } catch (error) {
+        expect(error.message).toBe('Unauthorized');
+      }
+    });
+
+    it('should call getNewAuthSecret', async () => {
+      await Query.twoFactorSecret!({}, {}, { injector, user } as any, {} as any);
+      expect(injector.get).toBeCalledWith(AccountsPassword);
+      expect(accountsPasswordMock.twoFactor.getNewAuthSecret).toBeCalledWith();
+    });
+  });
+});
