2c5746155fbd4cbc299b04eed21a2556d8938e4f
Agusti Fernandez <me@agusti.me>
2019-05-29 20:21:38 +0200

    Forgot to run prettier, passing test:prettier now, should pass CI

--- examples/graphql-server-typeorm-postgres/src/connect.ts
+++ examples/graphql-server-typeorm-postgres/src/connect.ts
@@ -1,14 +1,12 @@
 import { createConnection } from 'typeorm';
 
 export const connect = (url = process.env.DATABASE_URL) => {
-    return createConnection({
-        type: 'postgres',
-        url,
-        entities: [
-            ...require('@accounts/typeorm').entities,
-        ],
-        synchronize: true,
-    }).then(connection => {
-        return connection;
-    });
+  return createConnection({
+    type: 'postgres',
+    url,
+    entities: [...require('@accounts/typeorm').entities],
+    synchronize: true,
+  }).then(connection => {
+    return connection;
+  });
 };
--- examples/graphql-server-typeorm-postgres/src/index.ts
+++ examples/graphql-server-typeorm-postgres/src/index.ts
@@ -8,25 +8,25 @@ import { AccountsTypeorm } from '@accounts/typeorm';
 import { connect } from './connect';
 
 export const createAccounts = async () => {
-    const connection = await connect(process.env.TEST_DATABASE_URL);
-    // Like, fix this man!
-    const tokenSecret = 'ACCOUNTS_SECRET' || 'not very secret secret';
-    const db = new AccountsTypeorm({ connection, cache: 1000 });
-    const password = new AccountsPassword();
-    const accountsServer = new AccountsServer(
-        {
-            db,
-            tokenSecret,
-            siteUrl: 'http://localhost:3000',
-        },
-        { password }
-    );
-    // Creates resolvers, type definitions, and schema directives used by accounts-js
-    const accountsGraphQL = AccountsModule.forRoot({
-        accountsServer,
-    });
+  const connection = await connect(process.env.TEST_DATABASE_URL);
+  // Like, fix this man!
+  const tokenSecret = 'ACCOUNTS_SECRET' || 'not very secret secret';
+  const db = new AccountsTypeorm({ connection, cache: 1000 });
+  const password = new AccountsPassword();
+  const accountsServer = new AccountsServer(
+    {
+      db,
+      tokenSecret,
+      siteUrl: 'http://localhost:3000',
+    },
+    { password }
+  );
+  // Creates resolvers, type definitions, and schema directives used by accounts-js
+  const accountsGraphQL = AccountsModule.forRoot({
+    accountsServer,
+  });
 
-    const typeDefs = `
+  const typeDefs = `
 type PrivateType @auth {
 field: String
 }
@@ -52,33 +52,33 @@ _: String
 }
 `;
 
-    const resolvers = {
-        Query: {
-            publicField: () => 'public',
-            privateField: () => 'private',
-            privateType: () => ({
-                field: () => 'private',
-            }),
-        },
-    };
+  const resolvers = {
+    Query: {
+      publicField: () => 'public',
+      privateField: () => 'private',
+      privateType: () => ({
+        field: () => 'private',
+      }),
+    },
+  };
 
-    const schema = makeExecutableSchema({
-        typeDefs: mergeTypeDefs([typeDefs, accountsGraphQL.typeDefs]) as any,
-        resolvers: mergeResolvers([accountsGraphQL.resolvers as any, resolvers]) as any,
-        schemaDirectives: {
-            ...accountsGraphQL.schemaDirectives,
-        },
-    });
+  const schema = makeExecutableSchema({
+    typeDefs: mergeTypeDefs([typeDefs, accountsGraphQL.typeDefs]) as any,
+    resolvers: mergeResolvers([accountsGraphQL.resolvers as any, resolvers]) as any,
+    schemaDirectives: {
+      ...accountsGraphQL.schemaDirectives,
+    },
+  });
 
-    // Create the Apollo Server that takes a schema and configures internal stuff
-    const server = new ApolloServer({
-        schema,
-        context: accountsGraphQL.context,
-    });
+  // Create the Apollo Server that takes a schema and configures internal stuff
+  const server = new ApolloServer({
+    schema,
+    context: accountsGraphQL.context,
+  });
 
-    server.listen(4000).then(({ url }) => {
-        // tslint:disable-next-line:no-console
-        console.log(`🚀  Server ready at ${url}`);
-    });
+  server.listen(4000).then(({ url }) => {
+    // tslint:disable-next-line:no-console
+    console.log(`🚀  Server ready at ${url}`);
+  });
 };
 createAccounts();
