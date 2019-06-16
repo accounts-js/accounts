16ec9066b191712506c4e1b21f75d7e30ff06ff4
Steven Kissack <steven.kissack@gmail.com>
2019-06-16 16:01:04 -0500

    fix misspelling of first name

--- examples/react-rest-typescript/src/Signup.tsx
+++ examples/react-rest-typescript/src/Signup.tsx
@@ -80,7 +80,7 @@ class Signup extends React.Component<WithStyles<'formContainer'> & RouteComponen
           Sign up
         </Typography>
         <FormControl margin="normal">
-          <InputLabel htmlFor="firstName">Fist name</InputLabel>
+          <InputLabel htmlFor="firstName">First name</InputLabel>
           <Input id="firstName" value={firstName} onChange={this.onChangeFirstName} />
         </FormControl>
         <FormControl margin="normal">
