7d46de63a6ff1226b697d77c03bf543440c5c6d1
pradel <pradel.leo@gmail.com>
2019-07-25 12:18:54 +0200

    Update TwoFactor.tsx

--- examples/react-rest-typescript/src/TwoFactor.tsx
+++ examples/react-rest-typescript/src/TwoFactor.tsx
@@ -1,64 +1,53 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { Button, Typography, FormControl, InputLabel, Input } from '@material-ui/core';
 import QRCode from 'qrcode.react';
 
 import { accountsRest } from './accounts';
 
-interface State {
-  secret: any;
-  oneTimeCode: string;
-}
+const TwoFactor = () => {
+  const [secret, setSecret] = useState();
+  const [oneTimeCode, setOneTimeCode] = useState('');
 
-class TwoFactor extends React.Component<RouteComponentProps<{}>, State> {
-  state = {
-    secret: null as any,
-    oneTimeCode: '',
+  const fetchTwoFactorSecret = async () => {
+    const data = await accountsRest.getTwoFactorSecret();
+    setSecret(data.secret);
   };
 
-  async componentDidMount() {
-    this.onGetTwoFactorSecret();
-  }
+  useEffect(() => {
+    fetchTwoFactorSecret();
+  }, []);
 
-  onGetTwoFactorSecret = async () => {
-    const { secret } = await accountsRest.getTwoFactorSecret();
-    this.setState({ secret });
-  };
-
-  onChangeOneTimeCode = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ oneTimeCode: target.value });
-  };
-
-  onSetTwoFactor = async () => {
+  const onSetTwoFactor = async () => {
     try {
-      await accountsRest.twoFactorSet(this.state.secret, this.state.oneTimeCode);
+      await accountsRest.twoFactorSet(secret, oneTimeCode);
     } catch (err) {
       console.log(err);
       alert(err.message);
     }
   };
 
-  render() {
-    const { secret, oneTimeCode } = this.state;
-    if (!secret) {
-      return null;
-    }
-    return (
-      <div>
-        <Typography gutterBottom>Two-factor authentication</Typography>
-        <Typography gutterBottom>Backup code:</Typography>
-        <Typography gutterBottom>{secret.base32}</Typography>
-        <Typography gutterBottom>Use Google Authenticator for example</Typography>
-        <QRCode value={secret.otpauth_url} />
-        <Typography gutterBottom>Confirm with one-time code:</Typography>
-        <FormControl margin="normal">
-          <InputLabel htmlFor="one-time-code">One time code</InputLabel>
-          <Input id="one-time-code" value={oneTimeCode} onChange={this.onChangeOneTimeCode} />
-        </FormControl>
-        <Button onClick={this.onSetTwoFactor}>Submit</Button>
-      </div>
-    );
+  if (!secret) {
+    return null;
   }
-}
+  return (
+    <div>
+      <Typography gutterBottom>Two-factor authentication</Typography>
+      <Typography gutterBottom>Backup code:</Typography>
+      <Typography gutterBottom>{secret.base32}</Typography>
+      <Typography gutterBottom>Use Google Authenticator for example</Typography>
+      <QRCode value={secret.otpauth_url} />
+      <Typography gutterBottom>Confirm with one-time code:</Typography>
+      <FormControl margin="normal">
+        <InputLabel htmlFor="one-time-code">One time code</InputLabel>
+        <Input
+          id="one-time-code"
+          value={oneTimeCode}
+          onChange={e => setOneTimeCode(e.target.value)}
+        />
+      </FormControl>
+      <Button onClick={onSetTwoFactor}>Submit</Button>
+    </div>
+  );
+};
 
 export default TwoFactor;
