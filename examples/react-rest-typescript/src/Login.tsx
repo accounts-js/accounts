d407899f92f9aa8866bca4e8a4d53a953a81fa69
pradel <pradel.leo@gmail.com>
2019-07-25 12:03:23 +0200

    Update Login.tsx

--- examples/react-rest-typescript/src/Login.tsx
+++ examples/react-rest-typescript/src/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps, Link } from 'react-router-dom';
 import {
   withStyles,
@@ -23,79 +23,59 @@ const styles = () => ({
 const SignUpLink = (props: any) => <Link to="/signup" {...props} />;
 const ResetPasswordLink = (props: any) => <Link to="/reset-password" {...props} />;
 
-interface State {
-  email: string;
-  password: string;
-  code: string;
-  error: string | null;
-}
+const Login = ({ classes, history }: WithStyles<'formContainer'> & RouteComponentProps<{}>) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [code, setCode] = useState('');
+  const [error, setError] = useState<string | null>(null);
 
-class Login extends React.Component<WithStyles<'formContainer'> & RouteComponentProps<{}>, State> {
-  state = {
-    email: '',
-    password: '',
-    code: '',
-    error: null,
-  };
-
-  onChangeEmail = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ email: target.value });
-  };
-
-  onChangePassword = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ password: target.value });
-  };
-
-  onChangeCode = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ code: target.value });
-  };
-
-  onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    this.setState({ error: null });
+    setError(null);
     try {
       await accountsPassword.login({
         user: {
-          email: this.state.email,
+          email,
         },
-        password: this.state.password,
-        code: this.state.code,
+        password,
+        code,
       });
-      this.props.history.push('/');
+      history.push('/');
     } catch (err) {
-      this.setState({ error: err.message });
+      setError(err.message);
     }
   };
 
-  render() {
-    const { classes } = this.props;
-    const { email, password, code, error } = this.state;
-    return (
-      <form onSubmit={this.onSubmit} className={classes.formContainer}>
-        <Typography variant="display1" gutterBottom>
-          Login
-        </Typography>
-        <FormControl margin="normal">
-          <InputLabel htmlFor="email">Email</InputLabel>
-          <Input id="email" value={email} onChange={this.onChangeEmail} />
-        </FormControl>
-        <FormControl margin="normal">
-          <InputLabel htmlFor="password">Password</InputLabel>
-          <Input id="password" type="password" value={password} onChange={this.onChangePassword} />
-        </FormControl>
-        <FormControl margin="normal">
-          <InputLabel htmlFor="password">2fa code if enabled</InputLabel>
-          <Input id="code" value={code} onChange={this.onChangeCode} />
-        </FormControl>
-        <Button variant="raised" color="primary" type="submit">
-          Login
-        </Button>
-        {error && <FormError error={error!} />}
-        <Button component={SignUpLink}>Sign Up</Button>
-        <Button component={ResetPasswordLink}>Reset Password</Button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={onSubmit} className={classes.formContainer}>
+      <Typography variant="display1" gutterBottom>
+        Login
+      </Typography>
+      <FormControl margin="normal">
+        <InputLabel htmlFor="email">Email</InputLabel>
+        <Input id="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </FormControl>
+      <FormControl margin="normal">
+        <InputLabel htmlFor="password">Password</InputLabel>
+        <Input
+          id="password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </FormControl>
+      <FormControl margin="normal">
+        <InputLabel htmlFor="password">2fa code if enabled</InputLabel>
+        <Input id="code" value={code} onChange={e => setCode(e.target.value)} />
+      </FormControl>
+      <Button variant="raised" color="primary" type="submit">
+        Login
+      </Button>
+      {error && <FormError error={error!} />}
+      <Button component={SignUpLink}>Sign Up</Button>
+      <Button component={ResetPasswordLink}>Reset Password</Button>
+    </form>
+  );
+};
 
 export default withStyles(styles)(Login);
