cc0648907f944ec9fb0499006756d9c9a96005e7
Agusti Fernandez <me@agusti.me>
2019-09-17 16:38:12 +0200

    NextJS + SSR + cookieStorage

--- examples/next-graphql-typescript/pages/ssr.tsx
+++ examples/next-graphql-typescript/pages/ssr.tsx
@@ -1,14 +1,3 @@
-// import React from 'react';
-// import Layout from '../components/Layout';
-// import HomeSSR from '../components/HomeSSR';
-
-// const HomePage = () => (
-//   <Layout titleKey="Cruceritis">
-//     <HomeSSR />
-//   </Layout>
-// );
-
-// export default HomePage;
 import Layout from '../components/Layout';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
@@ -16,10 +5,22 @@ import cookies from 'next-cookies';
 import Router from 'next/router';
 import { accountsClient, accountsGraphQL } from '../utils/accounts';
 import { EmailRecord } from '@accounts/types';
+import { Button, Typography } from '@material-ui/core';
+
+async function onResendEmail() {
+  const { user } = this.state;
+  await accountsGraphQL.sendVerificationEmail(user.emails[0].address);
+}
+
+async function onLogout() {
+  await accountsClient.logout();
+  Router.push('/login');
+}
+
 function auth(ctx) {
   const allCookies = cookies(ctx);
-  // console.log(allCookies);
-  const token = allCookies.accounts_accessToken;
+  console.log(allCookies);
+  const token = allCookies['accounts:accessToken'];
   /*
    * If `ctx.req` is available it means we are on the server.
    * Additionally if there's no token it means the user is not logged in.
@@ -33,15 +34,11 @@ function auth(ctx) {
   if (!token) {
     Router.push('/login');
   }
-  // const user = await accountsGraphQL.getUser();
-  // const token = await user;
-  // console.log(token);
   return token;
 }
 
 async function getUser(token) {
   const user = await accountsGraphQL.getUser();
-  console.log(user);
   return user;
 }
 type User = {
@@ -49,46 +46,37 @@ type User = {
   id: string;
   emails: EmailRecord[];
 };
-const HomePage = (props: { token: string; user: User; username: string }) => (
-  <Layout titleKey={'Shows'}>
+const HomePage = (props: { token: string; user: User }) => (
+  <Layout titleKey="Home">
     {/* <h1>Batman TV Shows</h1> */}
-    <div>{props.username}</div>
-    <div>{props.token}</div>
+    <div></div>
+    <div>
+      <Typography gutterBottom={true}>Hello {props.user.username}, you are logged in</Typography>
+      <Typography>Your 🍪AccessToken is: {props.token}</Typography>
+      <Typography gutterBottom={true}>Email: {props.user.emails[0].address}</Typography>
+      <Typography gutterBottom={true}>
+        You email is {props.user.emails[0].verified ? 'verified' : 'unverified'}
+      </Typography>
+      {!props.user.emails[0].verified && (
+        <Button onClick={onResendEmail}>Resend verification email</Button>
+      )}
+      <Link href="two-factor">
+        <a>Set up 2fa</a>
+      </Link>
+      <Button variant="outlined" color="primary" onClick={onLogout}>
+        <a>Logout</a>
+      </Button>
+    </div>
   </Layout>
 );
 
 HomePage.getInitialProps = async function(ctx) {
   const token = auth(ctx);
   const user = await getUser(token);
-  const username = await user.username;
-  console.log(username);
-  //  user.username;
-  // if (user) {
-
-  // }
-  // await this.setState({ user });
-  // const getUser = await getUser(token);
-  // /*
-  //  * If `ctx.req` is available it means we are on the server.
-  //  * Additionally if there's no token it means the user is not logged in.
-  //  */
-  // if (ctx.req && !token) {
-  //   ctx.res.writeHead(302, { Location: '/login' });
-  //   ctx.res.end();
-  // }
-  // // We already checked for server. This should only happen on client.
-  // if (!token) {
-  //   Router.push('/login')
-  // }
-  // console.log({token});
-  // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  // const data = await res.json();
-  // console.log(`Show data fetched. Count: ${data.length}`);
 
   return {
     token: token,
-    user: (await user) as User,
-    // username: username,
+    user: user,
   };
 };
 
