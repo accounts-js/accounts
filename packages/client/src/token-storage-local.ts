2af6c19df71f6245bf0a460c3dad45c49a1708c4
Léo Pradel <pradel.leo@gmail.com>
2019-09-21 22:11:45 +0200

    Update TokenStorage to support non async storage (#810)

--- packages/client/src/token-storage-local.ts
+++ packages/client/src/token-storage-local.ts
@@ -1,15 +1,15 @@
 import { TokenStorage } from './types';
 
 export const tokenStorageLocal: TokenStorage = {
-  async setItem(key: string, value: string): Promise<void> {
+  setItem(key: string, value: string): void {
     return localStorage.setItem(key, value);
   },
 
-  async getItem(key: string): Promise<string | null> {
+  getItem(key: string): string | null {
     return localStorage.getItem(key);
   },
 
-  async removeItem(key: string): Promise<void> {
+  removeItem(key: string): void {
     return localStorage.removeItem(key);
   },
 };
