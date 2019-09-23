52ac28726f60fed6f81db73134ddc18f346284cf
ozsay <ozsayag@gmail.com>
2019-09-23 16:05:54 +0300

    add typing to resolver

--- packages/graphql-api/src/modules/accounts/resolvers/loginResult.ts
+++ packages/graphql-api/src/modules/accounts/resolvers/loginResult.ts
@@ -1,8 +1,14 @@
+import {
+  LoginWithServiceResultResolvers,
+  LoginResult as GeneratedLoginResult,
+  MfaLoginResult as GeneratedMfaLoginResult,
+} from '../../../models';
+
 export const LoginResult = {};
 
-export const LoginWithServiceResult = {
-  __resolveType(obj: any) {
-    if (obj.tokens) {
+export const LoginWithServiceResult: LoginWithServiceResultResolvers = {
+  __resolveType(obj: GeneratedLoginResult | GeneratedMfaLoginResult) {
+    if ((obj as GeneratedLoginResult).tokens) {
       return 'LoginResult';
     }
 
