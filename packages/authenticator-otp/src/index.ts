c08ce3661a6b4df0b2b58e1da28db24f8ca9f7ee
Pradel Léo <pradel.leo@gmail.com>
2019-09-23 22:09:12 +0200

    Update index.ts

--- packages/authenticator-otp/src/index.ts
+++ packages/authenticator-otp/src/index.ts
@@ -8,6 +8,7 @@ interface AuthenticatorService {
   setStore(store: DatabaseInterface): void;
   associate(userId: string, params: any): Promise<any>;
   authenticate(authenticatorId: string, params: any): Promise<boolean>;
+  // TODO ability to delete an authenticator
 }
 
 export interface AuthenticatorOtpOptions {
