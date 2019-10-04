c90c417d3dba12fe9a7ef010c1407abf83759ab6
Nick Bolles <me@nickbolles.com>
2019-10-04 03:53:00 +0000

    export GraphQLErrorList

--- packages/graphql-client/src/index.ts
+++ packages/graphql-client/src/index.ts
@@ -1,3 +1,3 @@
 export * from './graphql-client';
-export { default } from './graphql-client';
-export { default as AccountsGraphQLClient } from './graphql-client';
+export { default as AccountsGraphQLClient, default } from './graphql-client';
+export * from './GraphQLErrorList';
