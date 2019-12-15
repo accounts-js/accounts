ec659dad71ed9fec529ff806584238b37a2067ce
pradel <pradel.leo@gmail.com>
2019-12-15 18:01:19 +0100

    Login page

--- examples/react-rest-typescript/src/Login.tsx
+++ examples/react-rest-typescript/src/Login.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { RouteComponentProps, Link as RouterLink } from 'react-router-dom';
 import {
-  FormControl,
-  InputLabel,
-  Input,
   Button,
   Typography,
   Container,
@@ -12,19 +9,16 @@ import {
   CardContent,
   Divider,
   Link,
+  TextField,
+  Grid,
 } from '@material-ui/core';
-
+import { useFormik, FormikErrors } from 'formik';
 import { accountsPassword } from './accounts';
-import FormError from './components/FormError';
 
 const useStyles = makeStyles(theme => ({
   container: {
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
-  },
-  formContainer: {
-    display: 'flex',
-    flexDirection: 'column',
   },
   cardContent: {
     padding: theme.spacing(3),
@@ -42,62 +36,115 @@ const ResetPasswordLink = React.forwardRef<RouterLink, any>((props, ref) => (
   <RouterLink to="/reset-password" {...props} ref={ref} />
 ));
 
+interface LoginValues {
+  email: string;
+  password: string;
+  code: string;
+}
+
 const Login = ({ history }: RouteComponentProps<{}>) => {
   const classes = useStyles();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [code, setCode] = useState('');
-  const [error, setError] = useState<string | null>(null);
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setError(null);
-    try {
-      await accountsPassword.login({
-        user: {
-          email,
-        },
-        password,
-        code,
-      });
-      history.push('/');
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  const formik = useFormik<LoginValues>({
+    initialValues: {
+      email: '',
+      password: '',
+      code: '',
+    },
+    validate: values => {
+      const errors: FormikErrors<LoginValues> = {};
+      if (!values.email) {
+        errors.email = 'Required';
+      }
+      if (!values.password) {
+        errors.password = 'Required';
+      }
+      return errors;
+    },
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        await accountsPassword.login({
+          user: {
+            email: values.email,
+          },
+          password: values.password,
+          code: values.code,
+        });
+        history.push('/');
+      } catch (error) {
+        // TODO snackbar?
+        alert(error);
+      }
+      setSubmitting(false);
+    },
+  });
 
   return (
     <Container maxWidth="sm" className={classes.container}>
       <Card>
         <CardContent className={classes.cardContent}>
-          <form onSubmit={onSubmit} className={classes.formContainer}>
-            <Typography variant="h5" gutterBottom>
-              Sign in
-            </Typography>
-            <FormControl margin="normal">
-              <InputLabel htmlFor="email">Email</InputLabel>
-              <Input id="email" value={email} onChange={e => setEmail(e.target.value)} />
-            </FormControl>
-            <FormControl margin="normal">
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-              />
-            </FormControl>
-            <FormControl margin="normal">
-              <InputLabel htmlFor="password">2fa code if enabled</InputLabel>
-              <Input id="code" value={code} onChange={e => setCode(e.target.value)} />
-            </FormControl>
-            <Button variant="contained" color="primary" type="submit">
-              Login
-            </Button>
-            {error && <FormError error={error!} />}
+          <form onSubmit={formik.handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Typography variant="h5" gutterBottom>
+                  Sign in
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  fullWidth={true}
+                  id="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  error={Boolean(formik.errors.email && formik.touched.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Password"
+                  variant="outlined"
+                  fullWidth={true}
+                  type="password"
+                  id="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  error={Boolean(formik.errors.password && formik.touched.password)}
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="2fa code if enabled"
+                  variant="outlined"
+                  fullWidth={true}
+                  id="code"
+                  value={formik.values.code}
+                  onChange={formik.handleChange}
+                  error={Boolean(formik.errors.code && formik.touched.code)}
+                  helperText={formik.touched.code && formik.errors.code}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={formik.isSubmitting}
+                >
+                  Sign in
+                </Button>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Grid container justify="flex-end" alignContent="center">
+                  <Link component={ResetPasswordLink}>Lost your password?</Link>
+                </Grid>
+              </Grid>
+            </Grid>
+
             <Divider className={classes.divider} />
-            <Link component={SignUpLink}>Sign Up</Link>
-            <Link component={ResetPasswordLink}>Reset Password</Link>
+            <Link component={SignUpLink}>Don't have an account?</Link>
           </form>
         </CardContent>
       </Card>
