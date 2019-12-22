904d5bfaaee0f7ffc398d5b4f14995d2c4a6ccc1
pradel <pradel.leo@gmail.com>
2019-12-22 19:02:25 +0100

    Improve home

--- examples/react-rest-typescript/src/Home.tsx
+++ examples/react-rest-typescript/src/Home.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import {
-  Button,
   Typography,
   Container,
   makeStyles,
@@ -9,7 +8,11 @@ import {
   CardContent,
   CardHeader,
   Divider,
+  Tooltip,
+  IconButton,
 } from '@material-ui/core';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import SendIcon from '@material-ui/icons/Send';
 import { accountsClient, accountsRest } from './accounts';
 
 const useStyles = makeStyles(theme => ({
@@ -26,6 +29,17 @@ const useStyles = makeStyles(theme => ({
   },
   cardContent: {
     padding: theme.spacing(3),
+  },
+  emailItem: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  emailItemPart: {
+    display: 'flex',
+  },
+  emailItemDot: {
+    marginRight: theme.spacing(2),
   },
 }));
 
@@ -57,6 +71,7 @@ const Home = () => {
 
   const onResendEmail = async () => {
     await accountsRest.sendVerificationEmail(user.emails[0].address);
+    alert('Verification email sent');
   };
 
   if (!user) {
@@ -70,14 +85,30 @@ const Home = () => {
         <Divider />
         <CardContent className={classes.cardContent}>
           {user.emails.map((email: any, index: number) => (
-            <React.Fragment key={index}>
-              <Typography gutterBottom>
-                {email.address}: {email.verified ? 'verified' : 'unverified'}
-              </Typography>
+            <div key={index} className={classes.emailItem}>
+              <div className={classes.emailItemPart}>
+                <Tooltip
+                  arrow
+                  placement="top-start"
+                  title={
+                    email.verified ? 'Your email is verified' : 'You need to verify your email'
+                  }
+                >
+                  <FiberManualRecordIcon
+                    className={classes.emailItemDot}
+                    color={email.verified ? 'primary' : 'error'}
+                  />
+                </Tooltip>
+                <Typography>{email.address}</Typography>
+              </div>
               {!email.verified && (
-                <Button onClick={onResendEmail}>Resend verification email</Button>
+                <Tooltip arrow placement="top-end" title="Resend verification email">
+                  <IconButton aria-label="Send" onClick={onResendEmail}>
+                    <SendIcon />
+                  </IconButton>
+                </Tooltip>
               )}
-            </React.Fragment>
+            </div>
           ))}
         </CardContent>
       </Card>
