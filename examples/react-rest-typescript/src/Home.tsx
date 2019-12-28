b65560a1d2665b4195be5a62ad5782c3466f82ba
pradel <pradel.leo@gmail.com>
2019-12-28 17:12:05 +0100

    Expose a custom route to allow profile update

--- examples/react-rest-typescript/src/Home.tsx
+++ examples/react-rest-typescript/src/Home.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import {
   Typography,
   makeStyles,
@@ -7,12 +6,13 @@ import {
   CardContent,
   CardHeader,
   Divider,
-  Tooltip,
-  IconButton,
+  TextField,
+  Grid,
+  CardActions,
+  Button,
 } from '@material-ui/core';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-import SendIcon from '@material-ui/icons/Send';
-import { accountsClient, accountsRest } from './accounts';
+import { useFormik, FormikErrors } from 'formik';
+import { accountsClient } from './accounts';
 import { AuthenticatedContainer } from './components/AuthenticatedContainer';
 
 const useStyles = makeStyles(theme => ({
@@ -29,6 +29,9 @@ const useStyles = makeStyles(theme => ({
   cardContent: {
     padding: theme.spacing(3),
   },
+  cardActions: {
+    padding: theme.spacing(3),
+  },
   emailItem: {
     display: 'flex',
     alignItems: 'center',
@@ -42,75 +45,115 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+interface AccountDetailsValues {
+  firstName: string;
+  lastName: string;
+}
+
 const Home = () => {
   const classes = useStyles();
-  const history = useHistory();
-  const [user, setUser] = useState();
+  const formik = useFormik<AccountDetailsValues>({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+    },
+    validate: values => {
+      const errors: FormikErrors<AccountDetailsValues> = {};
+      if (!values.firstName) {
+        errors.firstName = 'Required';
+      }
+      if (!values.lastName) {
+        errors.lastName = 'Required';
+      }
+      return errors;
+    },
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        const tokens = await accountsClient.refreshSession();
+        if (tokens) {
+          await fetch(`${process.env.REACT_APP_API_URL}/user`, {
+            method: 'PUT',
+            headers: {
+              Authorization: tokens ? 'Bearer ' + tokens.accessToken : '',
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              firstName: values.firstName,
+              lastName: values.lastName,
+            }),
+          });
+          alert('Profile updated');
+        }
+      } catch (error) {
+        alert(error);
+      }
+      setSubmitting(false);
+    },
+  });
 
   useEffect(() => {
+    const fetchUser = async () => {
+      // refresh the session to get a new accessToken if expired
+      const tokens = await accountsClient.refreshSession();
+      if (tokens) {
+        const res = await fetch(`${process.env.REACT_APP_API_URL}/user`, {
+          headers: {
+            Authorization: tokens ? 'Bearer ' + tokens.accessToken : '',
+          },
+        });
+        const data = await res.json();
+        formik.setValues({ firstName: data.user.firstName, lastName: data.user.lastName });
+      }
+    };
+
     fetchUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const fetchUser = async () => {
-    // refresh the session to get a new accessToken if expired
-    const tokens = await accountsClient.refreshSession();
-    if (!tokens) {
-      history.push('/login');
-      return;
-    }
-    const res = await fetch('http://localhost:4000/user', {
-      headers: {
-        Authorization: tokens ? 'Bearer ' + tokens.accessToken : '',
-      },
-    });
-    const data = await res.json();
-    setUser(data.user);
-  };
-
-  const onResendEmail = async () => {
-    await accountsRest.sendVerificationEmail(user.emails[0].address);
-    alert('Verification email sent');
-  };
-
-  if (!user) {
-    return <AuthenticatedContainer>Loading ...</AuthenticatedContainer>;
-  }
   return (
     <AuthenticatedContainer>
       <Typography variant="h5">Account Details</Typography>
       <Divider className={classes.divider} />
       <Card className={classes.card}>
-        <CardHeader subheader="Emails" className={classes.cardHeader} />
-        <Divider />
-        <CardContent className={classes.cardContent}>
-          {user.emails.map((email: any, index: number) => (
-            <div key={index} className={classes.emailItem}>
-              <div className={classes.emailItemPart}>
-                <Tooltip
-                  arrow
-                  placement="top-start"
-                  title={
-                    email.verified ? 'Your email is verified' : 'You need to verify your email'
-                  }
-                >
-                  <FiberManualRecordIcon
-                    className={classes.emailItemDot}
-                    color={email.verified ? 'secondary' : 'error'}
-                  />
-                </Tooltip>
-                <Typography>{email.address}</Typography>
-              </div>
-              {!email.verified && (
-                <Tooltip arrow placement="top-end" title="Resend verification email">
-                  <IconButton aria-label="Send" onClick={onResendEmail}>
-                    <SendIcon />
-                  </IconButton>
-                </Tooltip>
-              )}
-            </div>
-          ))}
-        </CardContent>
+        <form onSubmit={formik.handleSubmit}>
+          <CardHeader subheader="Account Details" className={classes.cardHeader} />
+          <Divider />
+          <CardContent className={classes.cardContent}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  label="First name"
+                  variant="outlined"
+                  fullWidth={true}
+                  id="firstName"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange}
+                  error={Boolean(formik.errors.firstName && formik.touched.firstName)}
+                  helperText={formik.touched.firstName && formik.errors.firstName}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Last name"
+                  variant="outlined"
+                  fullWidth={true}
+                  id="lastName"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange}
+                  error={Boolean(formik.errors.lastName && formik.touched.lastName)}
+                  helperText={formik.touched.lastName && formik.errors.lastName}
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+          <Divider />
+          <CardActions className={classes.cardActions}>
+            <Button variant="contained" type="submit" disabled={formik.isSubmitting}>
+              Update profile
+            </Button>
+          </CardActions>
+        </form>
       </Card>
     </AuthenticatedContainer>
   );
