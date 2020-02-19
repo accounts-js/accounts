1e68c0539e1b63b213249d0a4deed5a2654d98af
pradel <pradel.leo@gmail.com>
2020-02-19 10:03:29 +0100

    docs: add search with algolia

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -17,6 +17,10 @@ module.exports = {
     repoUrl: 'https://github.com/accounts-js/accounts',
   },
   themeConfig: {
+    algolia: {
+      apiKey: 'c872074b5f5ecd186d94d7c19b27d757',
+      indexName: 'accounts',
+    },
     navbar: {
       logo: {
         alt: 'My Site Logo',
