7876b308d6b391e8b58298b85f49dffa35f4a9fb
Vlad Klimenkov <vlad.klimenkovv@gmail.com>
2020-03-05 14:17:51 +0300

    test: add tests for autologin

--- packages/rest-express/__tests__/endpoints/password/register.ts
+++ packages/rest-express/__tests__/endpoints/password/register.ts
@@ -1,4 +1,5 @@
 import { registerPassword } from '../../../src/endpoints/password/register';
+import { LoginResult } from '@accounts/types';
 
 const res: any = {
   json: jest.fn(),
@@ -75,6 +76,64 @@ describe('registerPassword', () => {
       username: 'toto',
     });
     expect(res.json).toHaveBeenCalledWith({ userId: null });
+    expect(res.status).not.toHaveBeenCalled();
+  });
+
+  it('should automatically login user after registration if enableAutologin flag is set to true', async () => {
+    const userId = '1';
+    const userEmail = 'test@test.com';
+
+    const passwordService = {
+      createUser: jest.fn(() => userId),
+    };
+
+    const createdUser = {
+      id: userId,
+      emails: [
+        {
+          address: userEmail,
+        },
+      ],
+    };
+
+    const loginResult = {
+      user: createdUser,
+      tokens: {
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+      },
+    } as LoginResult;
+
+    const accountsServer = {
+      options: { enableAutologin: true, ambiguousErrorMessages: false },
+      getServices: () => ({
+        password: passwordService,
+      }),
+      findUserById: jest.fn(() => createdUser),
+      loginWithUser: jest.fn(() => loginResult),
+    };
+    const middleware = registerPassword(accountsServer as any);
+
+    const req = {
+      body: {
+        user: {
+          email: userEmail,
+        },
+      },
+      headers: {},
+    };
+    const reqCopy = { ...req };
+
+    await middleware(req as any, res);
+
+    expect(req).toEqual(reqCopy);
+    expect(accountsServer.getServices().password.createUser).toHaveBeenCalledWith({
+      email: userEmail,
+    });
+    expect(res.json).toHaveBeenCalledWith({
+      userId,
+      loginResult,
+    });
     expect(res.status).not.toHaveBeenCalled();
   });
 
