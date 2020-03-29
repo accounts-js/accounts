97f01d160673c1e5f9f0f76691cc72094e87000c
Vlad Klimenkov <vlad.klimenkovv@gmail.com>
2020-03-29 23:29:33 +0300

    feat: add custom registration payload function

--- packages/oauth/src/types/oauth-options.ts
+++ packages/oauth/src/types/oauth-options.ts
@@ -3,5 +3,6 @@ import { OAuthUser } from './oauth-user';
 export interface OAuthOptions {
   [provider: string]: {
     authenticate: (params: any) => Promise<OAuthUser>;
+    getRegistrationPayload?: (oauthUser: OAuthUser) => Promise<any>;
   };
 }
