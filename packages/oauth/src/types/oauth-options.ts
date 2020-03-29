900157dedadcb7e479e8379dad1799e9fc8364ac
Vlad Klimenkov <vlad.klimenkovv@gmail.com>
2020-03-29 23:32:21 +0300

    docs: leave description to oauth options

--- packages/oauth/src/types/oauth-options.ts
+++ packages/oauth/src/types/oauth-options.ts
@@ -2,7 +2,13 @@ import { OAuthUser } from './oauth-user';
 
 export interface OAuthOptions {
   [provider: string]: {
+    /**
+     * Should return oauth user payload from passed authentication code or token
+     */
     authenticate: (params: any) => Promise<OAuthUser>;
+    /**
+     * Allows to provide function to create custom payload for user creation method based on data from OAuth API
+     */
     getRegistrationPayload?: (oauthUser: OAuthUser) => Promise<any>;
   };
 }
