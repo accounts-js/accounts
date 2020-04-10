66926435c0e5ebc4aa5b1c881f7526602e8078d2
Vlad Klimenkov <vlad.klimenkovv@gmail.com>
2020-04-11 01:23:03 +0300

    test: improve coverage

--- packages/oauth-twitter/__tests__/index.ts
+++ packages/oauth-twitter/__tests__/index.ts
@@ -4,4 +4,30 @@ describe('AccountsOAuthTwitter', () => {
   it('should have default export AccountsOAuthTwitter', () => {
     expect(typeof AccountsOAuthTwitter).toBe('function');
   });
+
+  describe('configuration', () => {
+    const twitterProvider = new AccountsOAuthTwitter({
+      key: 'TWITTER_KEY',
+      secret: 'TWITTER_SECRET',
+      getRegistrationPayload: async oauthUser => ({
+        email: oauthUser.email,
+        name: oauthUser.username,
+      }),
+    });
+
+    it('should allow providing function for getting custom payload for user registration', async () => {
+      const twitterExampleUser = {
+        id: '123',
+        username: 'twitteruser',
+        email: 'twitteruser@t.co',
+      };
+
+      expect(twitterProvider.getRegistrationPayload).not.toBeUndefined();
+
+      const userToRegister = await twitterProvider.getRegistrationPayload!(twitterExampleUser);
+
+      expect(userToRegister.email).toBe('twitteruser@t.co');
+      expect(userToRegister.name).toBe('twitteruser');
+    });
+  });
 });
