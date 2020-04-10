d3267c07ab4d023234065a8e2483f8fc6d3cbaab
Vlad Klimenkov <vlad.klimenkovv@gmail.com>
2020-04-10 18:32:23 +0300

    refactor: move OAuthProvider into specific interface

---
+++ packages/oauth/src/types/oauth-provider.ts
@@ -0,0 +1,12 @@
+import { OAuthUser } from './oauth-user';
+
+export interface OAuthProvider {
+  /**
+   * Should return oauth user payload from passed authentication code or token
+   */
+  authenticate: (params: any) => Promise<OAuthUser>;
+  /**
+   * Allows to provide function to create custom payload for user creation method based on data from OAuth API
+   */
+  getRegistrationPayload?: (oauthUser: OAuthUser) => Promise<any>;
+}
