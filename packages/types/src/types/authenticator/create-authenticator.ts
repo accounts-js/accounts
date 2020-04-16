177fd40f30723b597fd5093c768d838d7892d0cb
pradel <pradel.leo@gmail.com>
2020-04-16 11:05:00 +0200

    comments

--- packages/types/src/types/authenticator/create-authenticator.ts
+++ packages/types/src/types/authenticator/create-authenticator.ts
@@ -1,6 +1,15 @@
 export interface CreateAuthenticator {
+  /**
+   * Authenticator type
+   */
   type: string;
+  /**
+   * User id linked to this authenticator
+   */
   userId: string;
+  /**
+   * Is authenticator active
+   */
   active: boolean;
   [additionalKey: string]: any;
 }
--- packages/types/src/types/mfa-challenge/create-mfa-challenge.ts
+++ packages/types/src/types/mfa-challenge/create-mfa-challenge.ts
@@ -1,6 +1,18 @@
 export interface CreateMfaChallenge {
+  /**
+   * User id linked to this challenge
+   */
   userId: string;
+  /**
+   * Authenticator that will be used to resolve this challenge
+   */
   authenticatorId?: string;
+  /**
+   * Random token used to identify the challenge
+   */
   token: string;
+  /**
+   * If scope is set to associate, the authenticate function will verify the authenticator next time the user call it
+   */
   scope?: 'associate';
 }
