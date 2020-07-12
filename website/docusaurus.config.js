f3135122bacaf84ec30b5e3513d1c01885154094
pradel <pradel.leo@gmail.com>
2020-07-12 19:32:55 +0200

    add twitter

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 module.exports = {
   title: 'accounts-js',
   tagline: 'Fullstack authentication and accounts-management for Javascript',
@@ -57,6 +50,10 @@ module.exports = {
             {
               label: 'Slack',
               href: 'https://accounts-js.slack.com',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/accountsjs',
             },
           ],
         },
