6da8829666f5bb6abffeb890646da26c9a15b3e8
pradel <pradel.leo@gmail.com>
2020-08-07 15:35:56 +0200

    lint

--- packages/mfa/__tests__/accounts-mfa.ts
+++ packages/mfa/__tests__/accounts-mfa.ts
@@ -2,27 +2,29 @@ import { AccountsMfa } from '../src/accounts-mfa';
 
 describe('AccountsMfa', () => {
   describe('authenticate', () => {
-    it('should throw an error if mfaToken is not passed', () => {
+    it('should throw an error if mfaToken is not passed', async () => {
       const accountsMfa = new AccountsMfa({ factors: {} });
-      expect(accountsMfa.authenticate({} as any, {})).rejects.toThrowError('Invalid mfa token');
+      await expect(accountsMfa.authenticate({} as any, {})).rejects.toThrowError(
+        'Invalid mfa token'
+      );
     });
   });
 
   describe('challenge', () => {
-    it('should throw an error if mfaToken is not passed', () => {
+    it('should throw an error if mfaToken is not passed', async () => {
       const accountsMfa = new AccountsMfa({ factors: {} });
-      expect(accountsMfa.challenge(undefined as any, undefined as any, {})).rejects.toThrowError(
-        'Invalid mfa token'
-      );
+      await expect(
+        accountsMfa.challenge(undefined as any, undefined as any, {})
+      ).rejects.toThrowError('Invalid mfa token');
     });
   });
 
   describe('findUserAuthenticatorsByMfaToken', () => {
-    it('should throw an error if mfaToken is not passed', () => {
+    it('should throw an error if mfaToken is not passed', async () => {
       const accountsMfa = new AccountsMfa({ factors: {} });
-      expect(accountsMfa.findUserAuthenticatorsByMfaToken(undefined as any)).rejects.toThrowError(
-        'Invalid mfa token'
-      );
+      await expect(
+        accountsMfa.findUserAuthenticatorsByMfaToken(undefined as any)
+      ).rejects.toThrowError('Invalid mfa token');
     });
   });
 });
