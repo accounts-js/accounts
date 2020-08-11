e450d83310d52f4c59a69faf58611e0cbe253225
pradel <pradel.leo@gmail.com>
2020-08-11 10:15:49 +0200

    fix rest-express

--- packages/rest-express/src/endpoints/oauth/provider-callback.ts
+++ packages/rest-express/src/endpoints/oauth/provider-callback.ts
@@ -12,7 +12,7 @@ export const providerCallback = (
   options?: AccountsExpressOptions
 ) => async (req: express.Request, res: express.Response) => {
   try {
-    const loggedInUser = await accountsServer.loginWithService(
+    const authenticationResult = await accountsServer.loginWithService(
       'oauth',
       {
         ...(req.params || {}),
@@ -23,14 +23,14 @@ export const providerCallback = (
       req.infos
     );
 
-    if (options && options.onOAuthSuccess) {
-      options.onOAuthSuccess(req, res, loggedInUser);
+    if ('id' in authenticationResult && options && options.onOAuthSuccess) {
+      options.onOAuthSuccess(req, res, authenticationResult);
     }
 
-    if (options && options.transformOAuthResponse) {
-      res.json(options.transformOAuthResponse(loggedInUser));
+    if ('id' in authenticationResult && options && options.transformOAuthResponse) {
+      res.json(options.transformOAuthResponse(authenticationResult));
     } else {
-      res.json(loggedInUser);
+      res.json(authenticationResult);
     }
   } catch (err) {
     if (options && options.onOAuthError) {
