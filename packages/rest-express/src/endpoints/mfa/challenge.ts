6c061e3af8c7d2b01560b918a2b592191f546ba8
pradel <pradel.leo@gmail.com>
2020-08-12 11:44:54 +0200

    use req.infos

--- packages/rest-express/src/endpoints/mfa/challenge.ts
+++ packages/rest-express/src/endpoints/mfa/challenge.ts
@@ -1,24 +1,16 @@
 import * as express from 'express';
-import * as requestIp from 'request-ip';
 import { AccountsServer } from '@accounts/server';
 import { AccountsMfa } from '@accounts/mfa';
 import { sendError } from '../../utils/send-error';
-import { getUserAgent } from '../../utils/get-user-agent';
 
 export const challenge = (accountsServer: AccountsServer) => async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
-    const userAgent = getUserAgent(req);
-    const ip = requestIp.getClientIp(req);
-
     const { mfaToken, authenticatorId } = req.body;
     const accountsMfa = accountsServer.getService('mfa') as AccountsMfa;
-    const challengeResult = await accountsMfa.challenge(mfaToken, authenticatorId, {
-      userAgent,
-      ip,
-    });
+    const challengeResult = await accountsMfa.challenge(mfaToken, authenticatorId, req.infos);
     res.json(challengeResult);
   } catch (err) {
     sendError(res, err);
