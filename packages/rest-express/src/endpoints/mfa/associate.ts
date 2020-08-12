c0e2a0b3d5f5e881880518f9c53d0d7ec3facd74
pradel <pradel.leo@gmail.com>
2020-08-12 10:57:54 +0200

    feat(rest-express): create mfa endpoints

---
+++ packages/rest-express/src/endpoints/mfa/associate.ts
@@ -0,0 +1,53 @@
+import * as express from 'express';
+import * as requestIp from 'request-ip';
+import { AccountsServer } from '@accounts/server';
+import { AccountsMfa } from '@accounts/mfa';
+import { sendError } from '../../utils/send-error';
+import { getUserAgent } from '../../utils/get-user-agent';
+
+export const associate = (accountsServer: AccountsServer) => async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const userId: string | undefined = (req as any).userId;
+    if (!userId) {
+      res.status(401);
+      res.json({ message: 'Unauthorized' });
+      return;
+    }
+
+    const userAgent = getUserAgent(req);
+    const ip = requestIp.getClientIp(req);
+
+    const { type, params } = req.body;
+    const accountsMfa = accountsServer.getService('mfa') as AccountsMfa;
+    const mfaAssociateResult = await accountsMfa.associate(userId, type, params, {
+      userAgent,
+      ip,
+    });
+    res.json(mfaAssociateResult);
+  } catch (err) {
+    sendError(res, err);
+  }
+};
+
+export const associateByMfaToken = (accountsServer: AccountsServer) => async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const userAgent = getUserAgent(req);
+    const ip = requestIp.getClientIp(req);
+
+    const { mfaToken, type, params } = req.body;
+    const accountsMfa = accountsServer.getService('mfa') as AccountsMfa;
+    const mfaAssociateResult = await accountsMfa.associateByMfaToken(mfaToken, type, params, {
+      userAgent,
+      ip,
+    });
+    res.json(mfaAssociateResult);
+  } catch (err) {
+    sendError(res, err);
+  }
+};
