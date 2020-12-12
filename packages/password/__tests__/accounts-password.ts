8c33ab9736d1886c800ba9517f543650258cba9e
pradel <pradel.leo@gmail.com>
2020-12-12 16:24:16 +0100

    fix password tests

--- packages/password/__tests__/accounts-password.ts
+++ packages/password/__tests__/accounts-password.ts
@@ -240,10 +240,14 @@ describe('AccountsPassword', () => {
     it('validate user email if enrolled', async () => {
       const findUserByResetPasswordToken = jest.fn(() => Promise.resolve(validUserEnroll));
       password.isTokenExpired = jest.fn(() => false);
+      const setPassword = jest.fn(() => Promise.resolve());
+      const removeAllResetPasswordTokens = jest.fn(() => Promise.resolve());
       const invalidateAllSessions = jest.fn(() => Promise.resolve());
       const verifyEmail = jest.fn(() => Promise.resolve());
       password.setStore({
         findUserByResetPasswordToken,
+        setPassword,
+        removeAllResetPasswordTokens,
         invalidateAllSessions,
         verifyEmail,
       } as any);
@@ -258,6 +262,8 @@ describe('AccountsPassword', () => {
       } as any;
       set(password.server, 'options.emailTemplates', {});
       await password.resetPassword(token, newPassword, connectionInfo);
+      expect(setPassword.mock.calls.length).toBe(1);
+      expect(removeAllResetPasswordTokens.mock.calls.length).toBe(1);
       expect(verifyEmail.mock.calls.length).toBe(1);
       expect(invalidateAllSessions.mock.calls[0]).toMatchSnapshot();
     });
@@ -265,9 +271,13 @@ describe('AccountsPassword', () => {
     it('reset password and invalidate all sessions', async () => {
       const findUserByResetPasswordToken = jest.fn(() => Promise.resolve(validUser));
       const isTokenExpired = jest.fn(() => false);
+      const setPassword = jest.fn(() => Promise.resolve());
+      const removeAllResetPasswordTokens = jest.fn(() => Promise.resolve());
       const invalidateAllSessions = jest.fn(() => Promise.resolve());
       password.setStore({
         findUserByResetPasswordToken,
+        setPassword,
+        removeAllResetPasswordTokens,
         invalidateAllSessions,
       } as any);
       const prepareMail = jest.fn(() => Promise.resolve());
@@ -284,6 +294,8 @@ describe('AccountsPassword', () => {
       set(password.server, 'options.emailTemplates', {});
       const loginResult = await password.resetPassword(token, newPassword, connectionInfo);
       expect(loginResult).toBeNull();
+      expect(setPassword.mock.calls.length).toBe(1);
+      expect(removeAllResetPasswordTokens.mock.calls.length).toBe(1);
       expect(invalidateAllSessions.mock.calls[0]).toMatchSnapshot();
     });
 
@@ -302,9 +314,13 @@ describe('AccountsPassword', () => {
         },
       };
       const loginWithUser = jest.fn(() => Promise.resolve(exampleLoginResult));
+      const setPassword = jest.fn(() => Promise.resolve());
+      const removeAllResetPasswordTokens = jest.fn(() => Promise.resolve());
       const invalidateAllSessions = jest.fn(() => Promise.resolve());
       tmpAccountsPassword.setStore({
         findUserByResetPasswordToken,
+        setPassword,
+        removeAllResetPasswordTokens,
         invalidateAllSessions,
       } as any);
       const prepareMail = jest.fn(() => Promise.resolve());
@@ -325,6 +341,8 @@ describe('AccountsPassword', () => {
         connectionInfo
       );
       expect(loginResult).toEqual(exampleLoginResult);
+      expect(setPassword.mock.calls.length).toBe(1);
+      expect(removeAllResetPasswordTokens.mock.calls.length).toBe(1);
       expect(invalidateAllSessions.mock.calls[0]).toMatchSnapshot();
     });
   });
