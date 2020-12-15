a9cf021782e49bc8544fd76b152d506a2463094d
Manuel <2084639+TeNNoX@users.noreply.github.com>
2020-12-15 12:24:09 +0000

    Add Context assumptions to generics definition

--- packages/graphql-api/src/utils/authenticated-resolver.ts
+++ packages/graphql-api/src/utils/authenticated-resolver.ts
@@ -1,6 +1,17 @@
-export const authenticated = <CustomRoot, CustomArgs, Context, Info, T>(
-  func: (root: CustomRoot, args: CustomArgs, context: Context, info: Info) => T | Promise<T>
-) => async (root: CustomRoot, args: CustomArgs, context: Context, info: Info) => {
+export const authenticated = <
+  CustomRoot,
+  CustomArgs,
+  CustomContext extends { skipJSAccountsVerification?: Boolean; userId?: any; user?: any },
+  Info,
+  ReturnType
+>(
+  func: (
+    root: CustomRoot,
+    args: CustomArgs,
+    context: CustomContext,
+    info: Info
+  ) => ReturnType | Promise<ReturnType>
+) => async (root: CustomRoot, args: CustomArgs, context: CustomContext, info: Info) => {
   if (context && context.skipJSAccountsVerification === true) {
     return func(root, args, context, info);
   }
