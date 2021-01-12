3c2264db9fa121677201c85730625ee4f27ba54c
Manuel <2084639+TeNNoX@users.noreply.github.com>
2021-01-12 23:06:10 +0100

    feat(graphql-api): custom typings for authenticated-resolver (#1004)
    Co-authored-by: Manuel <TeNNoX@users.noreply.github.com>

--- packages/graphql-api/src/utils/authenticated-resolver.ts
+++ packages/graphql-api/src/utils/authenticated-resolver.ts
@@ -1,9 +1,17 @@
-export const authenticated = (func: any) => async (
-  root: any,
-  args: any,
-  context: any,
-  info: any
-) => {
+export const authenticated = <
+  CustomRoot,
+  CustomArgs,
+  CustomContext extends { skipJSAccountsVerification?: Boolean; userId?: any; user?: any },
+  Info,
+  ReturnType
+>(
+  func: (
+    root: CustomRoot,
+    args: CustomArgs,
+    context: CustomContext,
+    info: Info
+  ) => ReturnType | Promise<ReturnType>
+) => async (root: CustomRoot, args: CustomArgs, context: CustomContext, info: Info) => {
   if (context && context.skipJSAccountsVerification === true) {
     return func(root, args, context, info);
   }
