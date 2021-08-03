920ccb674e0c2414f9fb141fcc87275cafcafb53
Pradel Léo <pradel.leo@gmail.com>
2021-08-03 18:40:26 +0200

    chore: upgrade react examples

--- examples/react-rest-typescript/src/index.tsx
+++ examples/react-rest-typescript/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import Router from './Router';
+import reportWebVitals from './reportWebVitals';
 
 const theme = createMuiTheme({
   palette: {
@@ -15,8 +16,16 @@ const theme = createMuiTheme({
 });
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <Router />
-  </ThemeProvider>,
-  document.getElementById('root') as HTMLElement
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+    ,
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
