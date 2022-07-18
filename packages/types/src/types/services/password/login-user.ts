a586d0f46161f83fe39aec86e6d871bc5b90ef4a
max_bezs <52088363+maxbezs@users.noreply.github.com>
2022-07-18 15:02:24 +0200

    Update login-user.ts

--- packages/types/src/types/services/password/login-user.ts
+++ packages/types/src/types/services/password/login-user.ts
@@ -3,11 +3,11 @@ import { LoginUserIdentity } from '../../login-user-identity';
 export interface LoginUserPasswordService {
   user: string | LoginUserIdentity;
   password: string;
-  receiveOther: boolean
-  receiveFastep: boolean
-  certifyAge: boolean
-  readTerms: boolean
-  allSelect: boolean
-  // 2FA code
+  receiveOther: boolean;
+  receiveFastep: boolean;
+  certifyAge: boolean;
+  readTerms: boolean;
+  allSelect: boolean;
+  // 2FA code;
   code?: string;
 }
