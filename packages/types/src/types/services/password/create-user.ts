b258fd950aa4f11868008e37036a941fc9b9ad45
max_bezs <52088363+maxbezs@users.noreply.github.com>
2022-07-18 14:58:38 +0200

    Update create-user.ts

--- packages/types/src/types/services/password/create-user.ts
+++ packages/types/src/types/services/password/create-user.ts
@@ -4,4 +4,9 @@ export interface CreateUserServicePassword extends CreateUser {
   username?: string;
   email?: string;
   password: string;
+  receiveOther: boolean
+  receiveFastep: boolean
+  certifyAge: boolean
+  readTerms: boolean
+  allSelect: boolean
 }
