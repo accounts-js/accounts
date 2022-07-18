124417ddb27d0cb7bdcb360352b75c6cceba9b87
max_bezs <52088363+maxbezs@users.noreply.github.com>
2022-07-18 15:04:34 +0200

    Update user.ts

--- packages/types/src/types/user.ts
+++ packages/types/src/types/user.ts
@@ -6,4 +6,9 @@ export interface User {
   id: string;
   services?: Record<string, any>;
   deactivated: boolean;
+  receiveOther: boolean;
+  receiveFastep: boolean;
+  certifyAge: boolean;
+  readTerms: boolean;
+  allSelect: boolean;
 }
