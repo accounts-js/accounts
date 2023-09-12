061b8797edf56e6ad3a41908e30071fd9f8aa63b
Niccolò Belli <niccolo.belli@linuxsystems.it>
2023-09-12 19:06:09 +0200

    docs: switch to docusaurus-plugin-typedoc-api

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,9 +1,15 @@
-// @ts-check
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
+const fs = require('fs');
+const path = require('path');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
   title: 'accounts-js',
   tagline: 'Fullstack authentication and accounts-management for Javascript',
   url: 'https://www.accountsjs.com',
@@ -16,79 +22,76 @@ const config = {
   customFields: {
     repoUrl: 'https://github.com/accounts-js/accounts',
   },
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      fathomAnalytics: {
-        siteId: 'NZPJJZTJ',
-        customDomain: 'https://goose.accountsjs.com',
+  themeConfig: {
+    fathomAnalytics: {
+      siteId: 'NZPJJZTJ',
+      customDomain: 'https://goose.accountsjs.com',
+    },
+    prism: {
+      theme: require('prism-react-renderer/themes/github'),
+      darkTheme: require('prism-react-renderer/themes/dracula'),
+    },
+    navbar: {
+      logo: {
+        alt: 'accounts.js logo',
+        src: 'img/logo.png',
       },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-      navbar: {
-        logo: {
-          alt: 'accounts.js logo',
-          src: 'img/logo.png',
-        },
 
-        items: [
-          { to: 'docs/introduction', label: 'Documentation', position: 'right' },
-          {
-            to: 'docs/api/server',
-            label: 'Api reference',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/accounts-js/accounts',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Introduction',
-                to: '/docs/introduction',
-              },
-              {
-                label: 'Api reference',
-                to: '/docs/api/server',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'GitHub Discussions',
-                href: 'https://github.com/accounts-js/accounts/discussions',
-              },
-              {
-                label: 'Slack',
-                href: 'https://accounts-js.slack.com',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/accountsjs',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} accounts-js`,
-      },
-    }),
+      items: [
+        { to: 'docs/introduction', label: 'Documentation', position: 'right' },
+        {
+          label: 'Api reference',
+          to: 'api',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/accounts-js/accounts',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Introduction',
+              to: '/docs/introduction',
+            },
+            {
+              label: 'Api reference',
+              to: 'api',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'GitHub Discussions',
+              href: 'https://github.com/accounts-js/accounts/discussions',
+            },
+            {
+              label: 'Slack',
+              href: 'https://accounts-js.slack.com',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/accountsjs',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} accounts-js`,
+    },
+  },
   presets: [
     [
       '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/accounts-js/accounts/edit/master/website/',
@@ -97,10 +100,43 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
-  plugins: [require.resolve('docusaurus-plugin-fathom')],
+  plugins: [
+    require.resolve('docusaurus-plugin-fathom'),
+    [
+      'docusaurus-plugin-typedoc-api',
+      {
+        projectRoot: `${__dirname}/..`,
+        changelogs: true,
+        packages: fs
+          .readdirSync(path.resolve(__dirname, '..', 'packages'))
+          .map((name) => `packages/${name}`)
+          .concat(
+            fs
+              .readdirSync(path.resolve(__dirname, '..', 'modules'))
+              .map((name) => `modules/${name}`)
+          ),
+        exclude: [
+          'packages/database-tests',
+          'packages/e2e',
+          'packages/error',
+          'packages/types',
+          'modules/module-magic-link',
+          'modules/module-password',
+        ],
+        typedocOptions: {
+          readme: 'none',
+          tsconfig: '../tsconfig.json',
+          excludeExternals: true,
+          excludePrivate: true,
+          excludeProtected: true,
+          excludeInternal: true,
+          externalPattern: '**/node_modules/*',
+          cleanOutputDir: true,
+        },
+      },
+    ],
+  ],
 };
-
-module.exports = config;
--- website/sidebars.js
+++ website/sidebars.js
@@ -1,38 +1,4 @@
-// @ts-check
-const { readdirSync, existsSync } = require('fs');
-const { resolve } = require('path');
-
-const packagesDir = resolve(__dirname, '..', 'packages');
-const modulesDir = resolve(__dirname, '..', 'modules');
-const apiDocsDir = resolve(__dirname, '..', 'website', 'docs', 'api');
-
-const dirs = readdirSync(apiDocsDir);
-
-/** @type any */
-const generatedApi = {};
-
-function resolvePackage(dir) {
-  let path = resolve(packagesDir, dir, 'package.json');
-  return existsSync(path) ? path : resolve(modulesDir, dir, 'package.json');
-}
-
-dirs.forEach((dir) => {
-  const packageName = require(resolvePackage(dir)).name;
-  const generatedSidebar = require(resolve(apiDocsDir, dir, 'sidebars'));
-  generatedApi[packageName] = [];
-  if (Object.keys(generatedSidebar.docs).length > 0) {
-    generatedApi[packageName] = Object.keys(generatedSidebar.docs).map((key) => ({
-      type: 'category',
-      label: key,
-      items: generatedSidebar.docs[key],
-    }));
-  }
-  generatedApi[packageName].unshift(`api/${dir}/globals`);
-  generatedApi[packageName].unshift(`api/${dir}/index`);
-});
-
-/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
-const sidebars = {
+module.exports = {
   docs: {
     Introduction: ['introduction', 'contributing'],
     'Getting started': ['getting-started', 'server', 'handling-errors', 'email', 'client'],
@@ -57,7 +23,4 @@ const sidebars = {
     ],
     Cookbook: ['cookbook/react-native'],
   },
-  api: generatedApi,
 };
-
-module.exports = sidebars;
